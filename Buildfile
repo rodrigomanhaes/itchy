0dd6c48722745243a3e8562b2e8d032494d5f1ce
Rodrigo Manhães <rmanhaes@gmail.com>
2011-06-28 18:31:27 -0300

    initial structure

---
+++ Buildfile
@@ -0,0 +1,7 @@
+repositories.remote << 'http://www.ibiblio.org/maven2'
+
+define 'itchy' do
+  project.version = '0.1.0'
+  package :jar
+end
+
---
+++ src/main/java/itchy/HelloItchy.java
@@ -0,0 +1,8 @@
+package itchy;
+
+public class HelloItchy {
+    public static void main(String[] args) {
+        System.out.println("I'm only a placeholder for making Git happy!");
+    }
+}
+
